a137e7cd853e4d1c411fa64d6904c07996551af0
panfen <2809956575@qq.com>
2017-05-18 22:32:38 +0800

    'update'

---
+++ pages/forum/forum.js
@@ -0,0 +1,31 @@
+Page({
+  data: {
+    areas1:[
+      {
+        title: '猫舍',
+        img: '../../images/pages/area1.jpg'
+      },
+      {
+        title: '猫粮',
+        img: '../../images/pages/area2.jpg'
+      },
+    ],
+    areas2:[
+      {
+        title: '工具',
+        img: '../../images/pages/area3.jpg'
+      },
+      {
+        title: '问答',
+        img: '../../images/pages/area4.jpg'
+      }
+    ],
+    topics:[
+      {
+        title:'寻找心爱的小橘猫',
+        desc:'5月18日下午失踪。疑似被男猫勾引走。快点回家吧，爸爸想你了',
+        img:'../../images/pages/topic1.jpg'
+      }
+    ]
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,26 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    imgUrls: [
+      '../../images/1495097539722.jpg',
+      '../../images/1495097548559.jpg',
+      '../../images/1495097552971.jpg'
+    ],
+    content:[
+      {
+        title:'长期吸猫有什么不良反应?',
+        cover:'../../images/pages/item1.jpg',
+        desc:'一日吸猫，终生想吸！这玩意儿可比鸦片厉害多了!'
+      },
+      {
+        title: '世间美猫42种',
+        cover: '../../images/pages/item2.jpg',
+        desc: '作为一个铲屎官，你知道多少种猫？快来来看看新的物种'
+      },
+      {
+        title: '惊！养了这么久的猫，今天才知道猫还有这技能！',
+        cover: '../../images/pages/item3.jpg',
+        desc: '愚蠢的两脚兽？没点能耐还能征服你们的星球？'
+      }
+    ]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
 })
