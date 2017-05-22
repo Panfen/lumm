346daeaed873aa62db746bb5296ece61d04a77ae
panfen <2809956575@qq.com>
2017-05-22 22:15:15 +0800

    'update'

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ App({
     }else{
       //调用登录接口
       wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
+        success: function (r) {
+          var code = r.code;
+          if(code){
+            wx.getUserInfo({
+              success: function (res) {
+                that.globalData.userInfo = res.userInfo
+                typeof cb == "function" && cb(that.globalData.userInfo)
+              }
+            });
+          }
         }
       })
     }
