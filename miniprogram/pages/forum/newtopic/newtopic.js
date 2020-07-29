de845ceca757ffa7b9240cf174570396ef580c8c
疾风 <2809956575@qq.com>
2020-07-29 20:41:11 +0800

    change: 发布话题

--- miniprogram/pages/forum/newtopic/newtopic.js
+++ miniprogram/pages/forum/newtopic/newtopic.js
@@ -5,7 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
+    topicText: '',
+    imgList: []
+  },
 
+  onTopicTextChange: function(e) {
+    this.setData({ topicText: e.detail.value })
+  },
+
+  chooseImage: function () {
+    wx.chooseImage({
+      complete: (res) => {
+        const { imgList } = this.data;
+        imgList.push(...res.tempFilePaths);
+        this.setData({ imgList })
+      },
+    })
+  },
+
+  onSubmitTopic: function () {
+    const { topicText, imgList } = this.data;
+    wx.getUserInfo({
+      complete: (res) => {
+        console.log(res)
+        const { avatarUrl, nickName } = res.userInfo;
+        const params = {
+          avatar: avatarUrl,
+          name: nickName,
+          open_id: '',
+          pics: imgList,
+          text: topicText,
+          type: 3
+        }
+        const db = wx.cloud.database();
+        db.collection('forum').add({
+          data: params,
+          success: res => {
+            wx.showToast({
+              title: '发布成功',
+            })
+            wx.navigateBack()
+          },
+          fail: err => {
+            wx.showToast({
+              title: '啊哦，发布失败了',
+            })
+          }
+        })
+      }
+    })
   },
 
   /**
@@ -15,52 +63,5 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
